058ec2d8d045619e1a40ffb7b87c56192986e01e
Rafael Schiavon <rafael.schiavon@encontrack.com>
2016-12-05 17:36:22 -0600

    Configuraciones con segmento .6. de kafka + OP34

---
+++ salida/cmia-sky-sun-op34/instala.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+echo ""
+echo "Script para instalar la instancia CMIA-SKY-SUN-OP34"
+echo ""
+
+if [ "$UID" != "0" ] ; then
+    echo "Este script debe ser ejecutado por root"
+    echo ""
+    exit -1
+fi
+
+# Crea los directorios de la instancia
+mkdir -p /siscom/instancias/CMIA-SKY-SUN-OP34/
+mkdir -p /siscom/instancias/CMIA-SKY-SUN-OP34/log/
+mkdir -p /siscom/instancias/CMIA-SKY-SUN-OP34/db/
+
+# Hacer que arranca.sh sea ejecutable
+chmod +x arranca.sh
+
+# Copia los archivos de la instancia
+cp configuracion.xml /siscom/instancias/CMIA-SKY-SUN-OP34/configuracion.xml
+cp arranca.sh        /siscom/instancias/CMIA-SKY-SUN-OP34/arranca.sh
+
+# Asignar el usuario al directorio de la instancia
+chown -R siscom:siscom /siscom/instancias/CMIA-SKY-SUN-OP34
+
+# Mueve el archivo de supervisord al directorio
+cp cmia-sky-sun-op34.ini /etc/supervisord.d
+
+echo "Terminamos!"
+echo ""
+
+exit 0
+
+#EOF
